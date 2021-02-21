655f81106bca5a8c677bdb4c4aa6e6d95df3b270
lorenzo-pc <lorenzo.ferrari@lsgalilei.org>
2021-02-21 18:23:52 +0100

    Add files via upload

---
+++ fluortanten.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+#pragma GCC optimize ("O3")
+#pragma GCC optimize ("Ofast")
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int64_t n;
+	cin >> n;
+
+	vector<int64_t> a;
+	for (int64_t i = 0, ai; i < n; i++) {
+		cin >> ai;
+		if (ai != 0)
+			a.push_back(ai);
+	}
+
+	int64_t mx = 0;
+	int64_t ans = 0;
+	int64_t suff = 0;
+	for (int64_t i = n - 2; i >= 0; i--) {
+		ans += (i + 1) * a[i];
+		suff += a[i];
+		mx = max(mx, suff);
+	}
+
+	cout << ans + mx << "\n";
+}
