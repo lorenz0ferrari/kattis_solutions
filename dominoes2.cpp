81ed35139040d13ddbe793ba00e22ce58730a833
lorenzo-ferrari <lorenzo.ferrari@lsgalilei.org>
2021-03-23 18:43:40 +0100

    .

---
+++ dominoes2.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+#pragma GCC optimize ("Ofast")
+using namespace std;
+
+#define F first
+#define S second
+#define int long long
+#define all(x) x.begin(),x.end()
+
+typedef pair<int, int> pii;
+
+
+vector<bool> vis;
+vector<vector<int>> g;
+
+void dfs(int v) {
+  vis[v] = true;
+
+  for (int u : g[v])
+    if (!vis[u])
+      dfs(u);
+}
+
+void solve() {
+  int n, m, l;
+  cin >> n >> m >> l;
+
+  vis.assign(n+1, false);
+  g.assign(n+1, vector<int> (0));
+
+  for (int i = 0, a, b; i < m; i++) {
+    cin >> a >> b;
+    g[a].push_back(b);
+  }
+
+  for (int i = 0, a; i < l; i++) {
+    cin >> a;
+    dfs(a);
+  }
+
+  int ans = 0;
+  for (int i = 1; i <= n; i++)
+    ans += vis[i];
+
+  cout << ans << endl;
+}
+
+int32_t main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  int t = 1;
+  cin >> t;
+  while (t--) {
+    solve();
+  }
+}
---
+++ islands3.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+#pragma GCC optimize("Ofast")
+using namespace std;
+
+const int N = 102;
+
+int n, m;
+char g[N][N];
+bool vis[N][N];
+
+void dfs(int i, int j, char c) {
+  vis[i][j] = true;
+
+  if (i > 0   && g[i-1][j  ] == c && !vis[i-1][j  ]) dfs(i-1, j  , c);
+  if (j > 0   && g[i  ][j-1] == c && !vis[i  ][j-1]) dfs(i  , j-1, c);
+  if (i < n-1 && g[i+1][j  ] == c && !vis[i+1][j  ]) dfs(i+1, j  , c);
+  if (j < m-1 && g[i  ][j+1] == c && !vis[i  ][j+1]) dfs(i  , j+1, c);
+}
+
+int32_t main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  cin >> n >> m;
+
+  for (int i = 0; i < n; i++)
+    for (int j = 0; j < m; j++)
+      cin >> g[i][j];
+
+  for (int i = 0; i < n; i++)
+    for (int j = 0; j < m; j++)
+      if (g[i][j] == 'L')
+        dfs(i, j, 'C');
+
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < m; j++) {
+      if (vis[i][j]) {
+        g[i][j] = 'L';
+        vis[i][j] = false;
+      }
+    }
+  }
+
+  int ans = 0;
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < m; j++) {
+      if (!vis[i][j] && g[i][j] == 'L') {
+        ans++;
+        dfs(i, j, 'L');
+      }
+    }
+  }
+
+  cout << ans << endl;
+}
