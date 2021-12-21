6b6e6c4727df83d56b6383b53aef3f240380620e
lorenzo-ferrari <lorenzo.ferrari@lsgalilei.org>
2021-12-21 13:04:58 +0100

    .

--- dominoes2.cpp
+++ dominoes2.cpp
@@ -1,56 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
+using LL = long long;
 
-#define F first
-#define S second
-#define int long long
-#define all(x) x.begin(),x.end()
-
-typedef pair<int, int> pii;
-
-
-vector<bool> vis;
-vector<vector<int>> g;
-
-void dfs(int v) {
+void dfs(int v, vector<vector<int>>& g, vector<bool>& vis) {
   vis[v] = true;
-
   for (int u : g[v])
     if (!vis[u])
-      dfs(u);
+      dfs(u, g, vis);
 }
 
 void solve() {
   int n, m, l;
   cin >> n >> m >> l;
-
-  vis.assign(n+1, false);
-  g.assign(n+1, vector<int> (0));
-
-  for (int i = 0, a, b; i < m; i++) {
+  vector<bool> vis(n + 1);
+  vector<vector<int>> g(n + 1);
+  for (int i = 0, a, b; i < m; ++i) {
     cin >> a >> b;
     g[a].push_back(b);
   }
-
-  for (int i = 0, a; i < l; i++) {
+  for (int i = 0, a; i < l; ++i) {
     cin >> a;
-    dfs(a);
+    dfs(a, g, vis);
   }
-
-  int ans = 0;
-  for (int i = 1; i <= n; i++)
-    ans += vis[i];
-
-  cout << ans << endl;
+  int cnt = 0;
+  for (bool x : vis)
+    cnt += x;
+  cout << cnt << "\n";
 }
 
-int32_t main() {
-  ios_base::sync_with_stdio(false);
-  cin.tie(NULL);
-
-  int t = 1;
-  cin >> t;
-  while (t--) {
+int main() {
+#ifdef LORENZO
+  freopen("input.txt", "r", stdin);
+  freopen("output.txt", "w", stdout);
+#endif
+  int t; cin >> t;
+  while (t--)
     solve();
-  }
 }
